7750fcf0e8c86284daafa6fd393282482aef2e3f
Honza Horak <hhorak@redhat.com>
2020-05-28 08:48:47 +0200

    Add imagestream testing and refactor the OCP testing to work with remote OCP4 cluster

--- test/test-lib-nginx.sh
+++ test/test-lib-nginx.sh
@@ -17,10 +17,20 @@ function test_nginx_integration() {
   local import_image=$3
   VERSION=$version ct_os_test_s2i_app "${image_name}" \
                                       "https://github.com/sclorg/nginx-container.git" \
-                                      examples/${version}/test-app \
-                                      "Test NGINX passed" \
-                                      8080 http 200 "" \
-                                      "${import_image}"
+                                      "examples/${version}/test-app" \
+                                      "Test NGINX passed"
 }
 
+# Check the imagestream
+function test_nginx_imagestream() {
+  case ${OS} in
+    rhel7|centos7) ;;
+    *) echo "Imagestream testing not supported for $OS environment." ; return 0 ;;
+  esac
+
+  ct_os_test_image_stream_s2i "${THISDIR}/imagestreams/nginx-${OS}.json" "${IMAGE_NAME}" \
+                              "https://github.com/sclorg/nginx-container.git" \
+                              "examples/${VERSION}/test-app" \
+                              "Test NGINX passed"
+}
 # vim: set tabstop=2:shiftwidth=2:expandtab:
