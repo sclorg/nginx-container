14b39a4d3f4a9365de62c9d39e5a9753342ec4c0
Yaakov Selkowitz <yselkowi@redhat.com>
2020-08-13 17:11:59 -0400

    Fix tests after renaming imagestream files

--- test/test-lib-nginx.sh
+++ test/test-lib-nginx.sh
@@ -26,7 +26,7 @@ function test_nginx_imagestream() {
     *) echo "Imagestream testing not supported for $OS environment." ; return 0 ;;
   esac
 
-  ct_os_test_image_stream_s2i "${THISDIR}/imagestreams/nginx-${OS}.json" "${IMAGE_NAME}" \
+  ct_os_test_image_stream_s2i "${THISDIR}/imagestreams/nginx-${OS%[0-9]*}.json" "${IMAGE_NAME}" \
                               "https://github.com/sclorg/nginx-container.git" \
                               "examples/${VERSION}/test-app" \
                               "Test NGINX passed"
