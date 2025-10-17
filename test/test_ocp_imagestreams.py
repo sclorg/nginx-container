175014e1eecb1e2d47f255a55e4973ffba5bf7e9
Petr "Stone" Hracek <phracek@redhat.com>
2025-10-17 10:23:06 +0200

    Fix issues that were suggested in pull request
    https://github.com/sclorg/nginx-container/pull/346
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- test/test_ocp_imagestreams.py
+++ test/test_ocp_imagestreams.py
@@ -6,12 +6,12 @@ from conftest import VARS
 class TestLatestImagestreams:
 
     def setup_method(self):
-        self.isc = ImageStreamChecker(working_dir=VARS.TEST_DIR / ".." / "..")
+        self.isc = ImageStreamChecker(working_dir=VARS.TEST_DIR.parent.parent)
 
     def test_latest_imagestream(self):
         """
         Test checks we have the latest imagestream in GitHub repository
         """
         self.latest_version = self.isc.get_latest_version()
-        assert self.latest_version != ""
+        assert self.latest_version
         self.isc.check_imagestreams(self.latest_version)
