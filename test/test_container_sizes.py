2412ea0b08b6c5cb832bfbf86c1409acf6b7a5f2
Petr "Stone" Hracek <phracek@redhat.com>
2026-04-20 12:16:02 +0200

    Compare nginx version against the major OS version.
    In case image is less then previous major OS version,
    then the test passed.
    
    In case image is higher then previous major OS version,
    then the test is skipped.
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

---
+++ test/test_container_sizes.py
@@ -0,0 +1,50 @@
+import pytest
+
+from container_ci_suite.container_lib import ContainerTestLib
+from container_ci_suite.compare_images import ContainerCompareClass
+from container_ci_suite.utils import get_public_image_name, get_previous_os_version
+
+from conftest import VARS
+
+
+class TestNginxContainerSizes:
+    """
+    Test container sizes
+    """
+
+    def setup_method(self):
+        """
+        Setup method
+        """
+        self.app = ContainerTestLib(image_name=VARS.IMAGE_NAME, s2i_image=True)
+
+    def teardown_method(self):
+        """
+        Teardown method
+        """
+        self.app.cleanup()
+
+    def test_compare_container_sizes(self):
+        """
+        Test the size of the Nginx container against the
+        already published container images.
+        """
+        published_image_name = get_public_image_name(
+            os_name=get_previous_os_version(VARS.OS),
+            base_image_name="postgresql",
+            version=VARS.VERSION,
+            stage_registry=True,
+        )
+        is_less_uncopressed = ContainerCompareClass.is_uncompressed_image_smaller(
+            built_image_name=VARS.IMAGE_NAME,
+            published_image=published_image_name,
+        )
+        is_less_compressed = ContainerCompareClass.is_compressed_image_smaller(
+            built_image_name=VARS.IMAGE_NAME,
+            published_image_name=published_image_name,
+        )
+        if not is_less_uncopressed or not is_less_compressed:
+            pytest.skip(
+                f"Container size is not less than the published image {published_image_name}. "
+                f"Uncompressed image size: {is_less_uncopressed}, Compressed image size: {is_less_compressed}"
+            )
