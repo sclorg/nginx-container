8d4430d741d1aa692635d2cc34d4b57090728617
Petr "Stone" Hracek <phracek@redhat.com>
2026-04-20 12:16:02 +0200

    Compare nginx version against the major OS version.
    In case image is less then previous major OS version,
    then the test passed.
    
    In case image is higher then previous major OS version,
    then the test is skipped.
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- test/test_container_basics.py
+++ test/test_container_basics.py
@@ -8,11 +8,20 @@ from conftest import VARS
 
 
 class TestNginxContainer:
+    """
+    Test container basics
+    """
 
     def setup_method(self):
+        """
+        Setup method
+        """
         self.app = ContainerTestLib(image_name=VARS.IMAGE_NAME, s2i_image=True)
 
     def teardown_method(self):
+        """
+        Teardown method
+        """
         self.app.cleanup()
 
     def test_run_s2i_usage(self):
@@ -26,41 +35,46 @@ class TestNginxContainer:
         """
         Test if container is runnable
         """
-        assert PodmanCLIWrapper.call_podman_command(
-            cmd=f"run --rm {VARS.IMAGE_NAME} &>/dev/null",
-            return_output=False
-        ) == 0
+        assert (
+            PodmanCLIWrapper.call_podman_command(
+                cmd=f"run --rm {VARS.IMAGE_NAME} &>/dev/null", return_output=False
+            )
+            == 0
+        )
 
     def test_scl_usage(self):
         """
         Test if nginx -v returns proper output
         """
 
-        assert f"nginx/{VARS.VERSION_NO_MICRO}" in PodmanCLIWrapper.podman_run_command_and_remove(
-            cid_file_name=VARS.IMAGE_NAME,
-            cmd="nginx -v"
+        assert (
+            f"nginx/{VARS.VERSION_NO_MICRO}"
+            in PodmanCLIWrapper.podman_run_command_and_remove(
+                cid_file_name=VARS.IMAGE_NAME, cmd="nginx -v"
+            )
         )
 
-    @pytest.mark.parametrize(
-        "dockerfile",
-        [
-            "Dockerfile",
-            "Dockerfile.s2i"
-        ]
-    )
+    @pytest.mark.parametrize("dockerfile", ["Dockerfile", "Dockerfile.s2i"])
     def test_dockerfiles(self, dockerfile):
         """
         Test if building nginx-container based on
         examples/Dockerfile works
         """
-        dp = DockerfileProcessor(dockerfile_path=f"{VARS.TEST_DIR}/examples/{dockerfile}")
-        dp.update_env_in_dockerfile(version=VARS.VERSION_NO_MICRO, what_to_replace="ENV NGINX_VERSION")
-        dp.update_variable_in_dockerfile(version=VARS.VERSION_NO_MICRO, variable="NGINX_VERSION")
+        dp = DockerfileProcessor(
+            dockerfile_path=f"{VARS.TEST_DIR}/examples/{dockerfile}"
+        )
+        dp.update_env_in_dockerfile(
+            version=VARS.VERSION_NO_MICRO, what_to_replace="ENV NGINX_VERSION"
+        )
+        dp.update_variable_in_dockerfile(
+            version=VARS.VERSION_NO_MICRO, variable="NGINX_VERSION"
+        )
         new_docker_file = dp.create_temp_dockerfile()
 
         assert self.app.build_test_container(
-            dockerfile=new_docker_file, app_url="https://github.com/sclorg/nginx-container.git",
-            app_dir="nginx-container"
+            dockerfile=new_docker_file,
+            app_url="https://github.com/sclorg/nginx-container.git",
+            app_dir="nginx-container",
         )
         assert self.app.test_app_dockerfile()
         cip = self.app.get_cip()
